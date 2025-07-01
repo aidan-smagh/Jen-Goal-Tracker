aa52c47b00e5cc239a832909d85bf5b43fd1c148
Aidan Smagh <aidansmagh@gmail.com>
2025-07-01 17:58:41 -0400

    Added function to read the data from a csv file

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
 
-print("Hello")
+#command line arguments for dynamic file names
+name = sys.argv[1]
+#print(name)
+
+def read_csv(file_name):
+    with open(name, mode = 'r') as file:
+        csvFile = csv.reader(file)
+        for lines in csvFile:
+            print(lines)
+
+read_csv(name)
